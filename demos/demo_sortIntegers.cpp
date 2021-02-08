5124c4d31da634ad864f77b1c353767751dd4cfa
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2021-02-07 22:45:18 -0300

    feat: Improving the demo_sortIntegers.cpp
    I made some changes in order to fill the array randomly. To check if
    the array was sorted, I made a simple function for this too.

--- demos/demo_sortIntegers.cpp
+++ demos/demo_sortIntegers.cpp
@@ -1,42 +1,51 @@
 #include <iostream>
+#include <random>
 #include "../src/heapsort.h"
 
-void printArr(int arr[], int size){
+void printArray(int arr[], int size);
+void fillArray(int arr[], int size);
+bool isSorted(int arr[], int size);
+
+int main(){
+
+	const int SIZE = 10;
+	int *arr = new int[SIZE];
+
+	fillArray(arr, SIZE);
+	std::cout << "The array before sorting :" << std::endl;
+	printArray(arr, SIZE);
+
+	Heapsort(arr, SIZE);
+	std::cout << std::endl <<"The array after sorting :" << std::endl;
+	printArray(arr, SIZE);
+
+        if(isSorted(arr, SIZE)) std::cout << std::endl << "The array has secessefuly sorted!" << std::endl;
+
+	delete[] arr;
+
+	return 0;
+}
+
+void printArray(int arr[], int size){
 	for(int i = 0; i < size; i++){
 		std::cout << arr[i] << " ";
 	}
 	std::cout << std::endl;
 }
 
-int main(){
+void fillArray(int arr[], int size){
 
-	const int SIZE = 15;
-	
-	int *arr = new int[SIZE];
-	arr[0] = 4;
-	arr[1] = 2;
-	arr[2] = 9;
-	arr[3] = 88;
-	arr[4] = 1;
-	arr[5] = -7;
-	arr[6] = 54;
-	arr[7] = 12;
-	arr[8] = 13;
-	arr[9] = 0;
-	arr[10] = -3;
-	arr[11] = 2;
-	arr[12] = 4;
-	arr[13] = 1;
-	arr[14] = 16;
+	std::mt19937 MersenneTwisterRand(time(0));
+	for(int i = 0; i < size; i++){
+		arr[i] = MersenneTwisterRand();	
+	}
+}
 
-	std::cout << "array before sorting :" << std::endl;
-	printArr(arr, SIZE);
-
-	Heapsort(arr, SIZE);
-	std::cout << "\narray after sorting :" << std::endl;
-	printArr(arr, SIZE);
-
-	delete[] arr;
-
-	return 0;
+bool isSorted(int arr[], int size){
+	for(int i = 1; i < size; i++){
+		if(arr[i - 1] >= arr[i]){
+			return false;
+		}
+	}
+	return true;
 }
